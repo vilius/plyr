fec2d824baa4dde5de432f594e09f1c7ac000a27
Vilius Paulauskas <viliusp@gmail.com>
2017-10-14 11:27:14 +0200

    Stable build with YouTube loop feature cherry-picked from develop.

--- src/js/plyr.js
+++ src/js/plyr.js
@@ -1840,6 +1840,14 @@
                         // 5    Video cued
                         switch (event.data) {
                             case 0:
+                                // YouTube doesn't support loop for a single video, so mimick it.
+                                if (config.loop) {
+                                    // YouTube needs a call to `stopVideo` before playing again
+                                    instance.stopVideo();
+                                    instance.playVideo();
+                                    break;
+                                }
+
                                 plyr.media.paused = true;
                                 _triggerEvent(plyr.media, "ended");
                                 break;
